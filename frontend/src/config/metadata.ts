36cf89ab99aa3c6fa8a6d38d83981c093332b916
thomaspfresco <thomasfresco@live.com>
2025-12-20 18:55:40 +0000

    fix seo

--- frontend/src/config/metadata.ts
+++ frontend/src/config/metadata.ts
@@ -1,11 +1,11 @@
 export const metadata = {
-  title: "LASTRO - O motor de busca da MPAGDP",
+  title: "LASTRO - O Motor de Busca da MPAGDP",
   description:
     "Uma nova forma de explorar música, tradição oral, memória coletiva e património humano.",
   keywords:
     "Lastro, MPAGDP, Inteligência Artificial, Música, Tradição Oral, Memória Coletiva, Património Humano, Cultura Portuguesa",
   author: "LASTRO",
-  url: "https://lastro.com/",
-  ogImage: "/og-image.jpg",
+  url: "https://lastro-v2.vercel.app",
+  ogImage: "https://lastro-v2.vercel.app/og-image.jpg",
   themeColor: "#080808",
 } as const;
